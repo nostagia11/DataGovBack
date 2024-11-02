a301ab46ab054ffc6472dae09ec82b53cad133fb
nostagia11 <hanine11said@gmail.com>
2024-11-02 19:08:35 +0100

    adding resources plugin

--- src/test/java/com/dats/datsconnexion/ColumnServiceTest.java
+++ src/test/java/com/dats/datsconnexion/ColumnServiceTest.java
@@ -9,13 +9,16 @@ import com.dats.datsconnexion.entities.Columns;
 import com.dats.datsconnexion.entities.Label;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.Optional;
 
-
+    @ExtendWith(MockitoExtension.class)
     public class ColumnServiceTest {
 
         @Mock
